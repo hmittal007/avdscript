4098498e9dda0cb7bf6b6ec434c238734007364f
hmittal007 <47351742+hmittal007@users.noreply.github.com>
2023-06-27 12:14:31 +0100

    updated
    updated

--- adjoin.ps1
+++ adjoin.ps1
@@ -7,5 +7,11 @@ param (
 )
 
 $SecurePWd = ConvertTo-SecureString $password -AsPlainText -Force
-$DomainJoinCred = New-Object System.Management.Automation.PSCredential ($username@$domainname, $SecurePWd )
-Add-Computer -Credential $DomainJoinCred -OUPath $oupath -DomainName $domainname -Server $servername 
+$DomainJoinCred = New-Object System.Management.Automation.PSCredential -ArgumentList ($username@$domainname,$SecurePWd)
+Add-Computer -Credential $DomainJoinCred -OUPath $oupath -DomainName $domainname -Server $servername -restart -force
+
+#
+#
+#$oupath="OU=Hcadpool,OU=VDI,OU=Azure,OU=O365-Sync,DC=corporate,DC=caresoftglobal,DC=com"
+#$cred=get-Credential
+#Add-Computer -Credential $cred -OUPath $oupath -DomainName corporate.caresoftglobal.com -Server apcorpdccind01.corporate.caresoftglobal.com -restart -force
