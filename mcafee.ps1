3cb9aa82ffa91c1a2eacf954b096c46ccd035734
hmittal007 <47351742+hmittal007@users.noreply.github.com>
2024-09-24 08:07:51 +0100

    Update mcafee.ps1

--- mcafee.ps1
+++ mcafee.ps1
@@ -1,2 +1,5 @@
-Set-Location 'C:\Packages\mcafee'
+Set-Location 'C:/Packages/mcafee'
 .\mcafee.exe /INSTALL=AGENT /SILENT /enableVDImode
+Write-Host "After installation"
+Start-Sleep -Seconds 30
+Exit
